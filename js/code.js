2cde25a52cc4632e4c817dd11add59fc05fcbf81
Mu-een <mueenslam@gmail.com>
2022-11-22 19:08:10 -0800

    update1

---
+++ js/code.js
@@ -0,0 +1,20 @@
+function calculator() {
+    let screen = document.querySelector('.screen');
+    let button = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+}
+
+// buttons.forEach(function(button)){
+//     button.addEventListener('click',(e)=>{
+//         let value = e.target.dataset.num;
+//         screen.value += value;
+//     })
+// }
+
+// function buttons () {
+//     buttons.forEach.addEventListener('click',(e)=>{
+//         let value = e.target.dataset.num;
+//         screen.value += value;
+//     })
+// }
